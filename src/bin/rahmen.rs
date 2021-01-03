e37f3f4c55b597925d0c659f8cb897fffa8e29b1
hartmut <hartmut@hksc.de>
2021-01-03 18:31:45 +0100

    Added usage and other updates to README.md

--- src/bin/rahmen.rs
+++ src/bin/rahmen.rs
@@ -35,16 +35,23 @@ use rahmen::provider::{
 use rahmen::provider_list::ListProvider;
 use rahmen::Timer;
 
+/// dataflow control, this is used as result R part
 #[derive(Copy, Clone, Eq, PartialEq)]
 enum RunControl {
+    /// terminate stream processing (by external command)
     Terminate,
+    /// stream processing encountered an error, but will continue
     Suppressed,
 }
 
+/// error handler for display stuff
 fn fatal_err<T>(result: RahmenResult<Option<T>>) -> RunResult<T> {
     match result {
+        // empty result means we terminate as planned (e.g. end of list)
         Ok(None) => Err(RunControl::Terminate),
+        // we process the result
         Ok(Some(t)) => Ok(t),
+        // display error and terminate
         Err(e) => {
             eprintln!("Encountered error, terminating: {}", e);
             Err(RunControl::Terminate)
@@ -52,8 +59,10 @@ fn fatal_err<T>(result: RahmenResult<Option<T>>) -> RunResult<T> {
     }
 }
 
+/// to keep running the dataflow when there's a stream error
 fn suppress_err<T>(result: RahmenResult<T>) -> RunResult<T> {
     result.map_err(|e| {
+        // just notify about error but keep processing
         eprintln!("Encountered error, suppressing: {}", e);
         RunControl::Suppressed
     })
@@ -109,6 +118,7 @@ fn main() -> RahmenResult<()> {
         .get_matches();
 
     let input = matches.value_of("input").expect("Input missing");
+    // box is used bec of dynamic typing for provider
     let mut provider: Box<dyn Provider<_>> = if input.eq("-") {
         println!("Reading from stdin");
         Box::new(ListProvider::new(BufReader::new(std::io::stdin())))
@@ -120,13 +130,6 @@ fn main() -> RahmenResult<()> {
         Box::new(rahmen::provider_glob::create(input)?)
     };
 
-    let allocator = timely::communication::allocator::Thread::new();
-    let mut worker = timely::worker::Worker::new(allocator);
-
-    let mut input: InputHandle<_, ()> = InputHandle::new();
-    let mut input_dimensions: InputHandle<_, (u32, u32)> = InputHandle::new();
-    let mut probe = ProbeHandle::new();
-
     let buffer_max_size: usize = matches
         .value_of("buffer_max_size")
         .expect("Missing buffer_max_size")
@@ -134,15 +137,28 @@ fn main() -> RahmenResult<()> {
         .unwrap();
 
     let font = Font::from_path(matches.value_of("font").unwrap(), 0).unwrap();
+
+    let time_str = matches.value_of("time").unwrap();
+    let delay = Duration::from_millis((f64::from_str(time_str).unwrap() * 1000f64) as u64);
+    println!("Delay: {:?}", delay);
+
+
+    // initialization for timely dataflow
+    let allocator = timely::communication::allocator::Thread::new();
+    let mut worker = timely::worker::Worker::new(allocator);
+
+
+    // input: #1 timeline #2 screen resolution
+    let mut input: InputHandle<_, ()> = InputHandle::new();
+    let mut input_dimensions: InputHandle<_, (u32, u32)> = InputHandle::new();
+    // to gather information about progress
+    let mut probe = ProbeHandle::new();
+
     let font_renderer = FontRenderer::with_font(font);
 
     let output = worker.dataflow(|scope| {
         let font_size = 30.;
 
-        let _last_time: Option<Instant> = None;
-        let time_str = matches.value_of("time").unwrap();
-        let delay = Duration::from_millis((f64::from_str(time_str).unwrap() * 1000f64) as u64);
-        println!("Delay: {:?}", delay);
         let stream = input.to_stream(scope).unary_frontier(
             timely::dataflow::channels::pact::Pipeline,
             "Ticker",
@@ -395,7 +411,7 @@ fn main() -> RahmenResult<()> {
             input_dimensions.send(display.dimensions());
         }
         input_dimensions.advance_to(now);
-        while probe.less_than(input.time()) {
+        while probe.less_than(&now) {
             worker.step();
         }
         match output.try_iter().all(|result| match result {
