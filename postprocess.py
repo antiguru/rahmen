f725c81a2c6c8656ab789f2eba2af3bfdb956eb7
hartmut <hartmut@hksc.de>
2021-02-22 22:12:28 +0100

    public version of postprocess/config

--- postprocess.py
+++ postprocess.py
@@ -1,46 +1,85 @@
-# python code that takes the metadata tags, after they have
-# been processed using the regex definitions and been joined by the separator,
+import re
+
+# python code that takes a list of the metadata tags, after they have
+# been processed using the regex definitions,
 # and processes them accordingly. It is currently required that this will return
 # a list of items (strings).
 #
-# The output will be unconditionally cleaned of empties and uniquified (so you should probably
-# set 'uniquify' and 'hide_empty' to false to have consistency in your input).
+# This File assumes some regex processing being done in the configuration file (see rahmen.toml example)
 #
-# this holds the item postions we want to drop
+# We used a positional approach here. We know how many tags we configured to be shown in the configuration file;
+# thus we can move back or forth from any given item and find another that way.
+# Let's say we have defined the field structure Info, Quarter, District_or_City, ProvinceState, Country, Date, Creator,
+# and we have looked for 'USA' and got the index for it. Assuming that 'USA' is in the 'country' field
+# (you'll have to take care that you choose your search terms carefully so that false positives are ruled out),
+# the structure is then Info, Quarter, District_or_City, ProvinceState, USA, Date, Creator
+# and the offsets:      ^^^⁻4,^^^-3,   ^^^-2,            ^^^-1          ^^^  ^^^+1 ^^^+2
+# so the 'info' field would be at index-4, the 'creator' field at index+2
+#
+# The output will be unconditionally cleaned of empties and uniquified unless you return a list of just one item
+# (see the example at the end)
+#
+# this holds the item positions we want to drop
 # dropping cannot be done ad hoc because it would shift the positions
 delx = []
 
 
+def modify(items, ix_to_check, val_to_check, ix_to_modify, val_to_modify, ix_to_delete=None):
+    # modify an item in items if it is not set depending on s/th matching another item,
+    # optionally mark item for deletion
+    #
+    # if this item matches the control value, and
+    if items[ix_to_check] == val_to_check:
+        # if the item to be modified is not set,
+        if not items[ix_to_modify]:
+            # modify it
+            items[ix_to_modify] = val_to_modify
+        # optionally mark item[ix] for deletion
+        if ix_to_delete:
+            delx.append(ix_to_delete)
+
+
 def pp_s_korea(items, it, ix):
-    # look for the item before the country ('Südkorea'), it's ProvinceState
-    # the structure is then Info, Quarter, District_or_City, ProvinceState, Südkorea, Date, Creator
-    # the offsets:                ^^^-3    ^^^-2             ^^^-1     ^^^we start here
+    # look for the item before the country ('South Korea'), it's ProvinceState
+    # the structure is then Info, Quarter, District_or_City, ProvinceState, South Korea, Date, Creator
+    # the offsets:          ^^^⁻4,^^^-3,   ^^^-2,            ^^^-1          ^^^we start here
     # the following assumes that the province suffix '-do' has already been regexed away
     #
-    # except in the case of Jeju, do this:
-    if items[ix - 1] != "Jeju":
-        # ...in the big cities, the name of the province is the well-known city name, so keep it
-        if items[ix - 1] in ["Seoul", "Busan"]:
-            # ...but drop the city district
-            delx.append(ix - 2)
-        else:
-            # ...otherwise drop the province
-            delx.append(ix - 1)
-    # always drop the district quarter
-    delx.append(ix - 3)
+    # ...in the big cities  and in Jeju, the name of the province is the well-known name, so keep it
+    if items[ix - 1] not in ["Seoul", "Jeju", "Busan"]:
+        # ...otherwise drop the province
+        delx.append(ix - 1)
+    # cut away city quarter overkill
+    quarter_parts = items[ix - 3].split(' ')
+    if len(quarter_parts) > 1:
+        items[ix - 3] = quarter_parts[0]
+    # set some landmark names from the district quarter
+    # TODO we could move this to a dict
+    modify(items, ix - 3, 'Sanga', ix - 4, 'Woryeonggyo Bridge', ix - 3)
+    modify(items, ix - 3, 'Pungcheon', ix - 4, 'Hahoe/Byeongsanseowon', ix - 3)
+    modify(items, ix - 3, 'Jinhyeon', ix - 4, 'Bulguksa/Seokguram', ix - 3)
+    modify(items, ix - 3, 'Cheongnyong', ix - 4, 'Beomeosa', ix - 3)
     return items
 
 
 def pp_morocco(items, it, ix):
-    # drop the province
-    delx.append(ix - 1)
+    # drop the province, except when it's Marrake([s|c]h)
+    if not 'Marrakech' in items[ix - 1]:
+        delx.append(ix - 1)
+    # set some landmark names from the city
+    modify(items, ix - 2, "M'Semrir", ix - 4, 'Gorges du Dades')
+    modify(items, ix - 2, "Zerkten", ix - 4, "Tizi n'Tichka")
+    modify(items, ix - 2, "Mezguita", ix - 4, "Tamnougalt")
+    modify(items, ix - 2, "Ikniouen", ix - 4, "Jbel Saghro")
+
     return items
 
 
 # cantons and abbreviations for them, to be extended
-cantons = {'Zürich': 'ZH', 'Basel-Stadt': 'BS'}
+cantons = {'Zürich': 'ZH', 'Basel-Stadt': 'BS', 'St. Gallen': 'SG'}
 
 
+# Someplace, Canton of Zürich, => Someplace ZH, unless Someplace in 'Canton of Zürich'
 def pp_ch_cantons(items, ix):
     ct = ''
     for canton in cantons.keys():
@@ -53,79 +92,130 @@ def pp_ch_cantons(items, ix):
             if city not in canton:
                 # append the canton's abbreviation
                 ct = ' ' + cantons.get(canton)
-            # mark city field for deletion
+            # mark city and country field for deletion
+            delx.append(ix)
             delx.append(ix - 2)
             # update input_canton field with city + abbreviation (or '')
             items[ix - 1] = city + ct
-
-
-def pp_ch(items, it, ix):
-    # Someplace, Kanton Zürich, => Someplace ZH, unless Someplace in 'Kanton Zürich'
-    pp_ch_cantons(items, ix)
     return items
 
 
-def pp_mark(items, it, ix):
-    # Someplace, Mark, => Someplace (Mark),
-    # get location
-    loc = items[ix - 1]
-    # drop it
-    delx.append(ix - 1)
-    # assign new content to province item
-    items[ix] = loc + ' ' + ''.join(['(', it, ')'])
-    return items
+# This defines timespans per country (province/state,(city, (...)).
+# We use only the key values, the value for the last key has to be None.
+# { 'Start date': {'End date': {'Country': None}}, ... }
+# This matches the metatags that are defined in the config file, but the order is reversed here.
+# This example assumes that the config file metatags amount to
+# Name, Sublocation, Location, ProvinceState, Country, Date, Creator
+# We match between and including start and end dates and proceed left from there: Country, ProvinceState, etc.
+# Date format here is YYYYMMDD (unlike in the display and config file).
+# This way, un-geotagged images will be associated with the country or places you visited
+# To skip items (leave them untouched), insert an empty string.
+# Existing entries will not be overwritten.
+# The start date has to be unique. Do not overlap end dates, when they do, the entry starting first wins.
+timespans = {
+    # 8-12-2012 to 8-14-2012, photos were taken in NY City
+    '20120812': {'20120814': {'USA': {'NY': {'New York': None}}}},
+    # 8-15-2012 to 8-28-2012, photos were taken in the Catskills
+    '20120815': {'20120828': {'USA': {'NY': {'': {'In the Catskills': None}}}}},
+    # 10-19-2013, photos were taken at Pyramid Lake
+    '20131019': {'20131019': {'USA': {'NV': {'Pyramid Lake': None}}}},
+    # from 4-20-2019 to 5-22-2019, photos are from the US
+    '20190420': {'20190522': {'USA': None}},
+    # from 1-10-2020 to 1-22-2020, photos are from Portugal
+    '20200110': {'20200122': {'Portugal': None}},
+    # 10-16 and 10-28-2020, Photos were repro'd from slides
+    '20201016': {'20201016': {'': {'': {'': {'': {'From Slide': None}}}}}},
+    '20201028': {'20201028': {'': {'': {'': {'': {'From Slide': None}}}}}},
+
+}
 
 
-# this defines timespans per country
-# { 'Start date': {'End date': 'Country'}, ... }
-# date format is YYYYMMDD
-# this way, un-geotagged images will be associated with the country you visited
-timespans = {'20170210':{'20170222':'Portugal'}, '20140925':{'20141101':'USA'}}
-
-# associate an image date with the country if it is in the associated timespan
-def pp_country_from_timespan(items):
-    # we assume that date is the item before the last and that it's formatted d.m.yyyy
-    # and that country is the item before date
-    # this has to be configured that way in the configuration file
-    # no real error checking is being done here
-    # the next two conditionals should catch crashes from missing indices
-    # we need at least country|date|something, so more than two items
-    if len(items) > 2:
-        # if there's no country information
-        if not items[len(items) -3]:
-           # get the strings for day, month, year (input format d.m.yyyy)
-            i_date_list = items[len(items) -2].split('.')
+# consume the rest of the timespan after country
+def pp_consume_timespan(key_list, items, pos):
+    # build the timespans dict access for the current key from the key list
+    # if there are more items in the timespan than items configured, we stop, but that's ok as it is an error.
+    # TODO there might be a better way than using eval...
+    # this gives timespans[key][key][...]...
+    eval_base = "timespans['" + "']['".join(key_list) + "']"
+    # now look for the key
+    for next_key in eval(eval_base + ".keys()"):
+        # move our pointer
+        pos = pos + 1
+        # we're going backward
+        i_pos = len(items) - pos
+        # unfortunately, lists wrap, we have to take care of that, otherwise we might overwrite values
+        # when we find too many items in a timespan entry, we stop and throw an alert
+        if i_pos < 0:
+            raise ValueError('Too many items in timespan: ' + str(key_list) + ' >>> ' + next_key)
         else:
-            # too few items, check instructions in the configuration file; we do nothing
-            return items
-        if len(i_date_list) != 3:
-            # this is not a correct date, do nothing.
-            return items
-        # convert the date string to YYYYMMDD
-        i_date = i_date_list[2]+i_date_list[1].zfill(2)+i_date_list[0].zfill(2)
-        for start_date in timespans.keys():
-            if i_date >= start_date:
-                for end_date in timespans[start_date].keys():
-                    if i_date <= end_date:
-                        # this assumes the country is before the date
-                        items[len(items) -3] = timespans[start_date].get(end_date)
-    return(items)
+            # item not set?
+            if not items[i_pos]:
+                # let's set it.
+                items[i_pos] = next_key
+            # is there another key in the queue? Otherwise, we're done.
+            if eval(eval_base + ".get('" + next_key + "')") is not None:
+                # if there is, do it again for the next key
+                key_list.append(next_key)
+                items = pp_consume_timespan(key_list, items, pos)
+    return items
 
 
-# primitive global replacements: the dictionary has keys (to look up) and replacement values.
-# these will be replaced wherever they occur
-# only literal keys are allowed, no regular expressions.
+# add information to image if the image data is inside a timespan
+def pp_metadata_from_timespan(items):
+    # we assume that 'date' is the item before the last (hence the pos[ition] is set to 2 below,
+    # lenght(items)-pos pointing to that place) and that it's formatted d.m.yyyy
+    # and that 'country' is the item before date
+    # this has to be configured that way in the configuration file
+    # no real error checking is being done here, but
+    # the conditionals should catch crashes from missing indices
+    pos = 2
+    # we need at least country|date|something, so more than two items
+    if len(items) > pos:
+        # get the strings for day, month, year (input format yyyy-mm-dd)
+        i_date_list = items[len(items) - pos].split('-')
+        print(i_date_list, len(i_date_list))
+        # without 3 items, it's not a correct date
+        if len(i_date_list) == 3:
+            # convert the date string to YYYYMMDD (add leading zeros if necessary)
+            i_date = i_date_list[2] + i_date_list[0].zfill(2) + i_date_list[1].zfill(2)
+            # we look for our dates
+            for start_date in timespans.keys():
+                if i_date >= start_date:
+                    # 'for' assigns an anonymous key to a variable, which is what we need, even if
+                    # there will be only one key
+                    for end_date in timespans[start_date].keys():
+                        if i_date <= end_date:
+                            key_list = [start_date, end_date]
+                            # now we consume the timespan data
+                            items = pp_consume_timespan(key_list, items, pos)
+    return (items)
+
+
+# for slides we delete all info that may have been set from the camera's GPS
+def pp_dia(ix):
+    for i in [1, 2, 3, 4, 5]:
+        delx.append(ix + 1)
+
+
+# global replacements: the dictionary has keys (to look up) and replacement values.
 def pp_glob(items, glob_replacements):
     for i, it in enumerate(items):
-        for k in glob_replacements.keys():
+        for key, value in glob_replacements.items():
             # update the working value to prevent regressions when multiple matches occur
-            it = it.replace(k, glob_replacements.get(k))
+            it = re.sub(key, value, it)
             items[i] = it
     return items
 
 
 # value/replacement dictionary
-glob_replacements = {'Zurich': 'Zürich', ' City': '', ' Township': '', ' Province': ''}
+# these will be replaced wherever they occur
+# regular expressions [https://docs.python.org/3/library/re.html] are allowed
+glob_replacements = {'Zurich': 'Zürich',
+                     ' City': '',
+                     ' Township': '',
+                     ' District': '',
+                     ' Province': '',
+                     }
 
 
 # main filter
@@ -136,26 +226,30 @@ def postprocess(items: [str], sep: str) -> str:
     print(items)
     # first, replace the global stuff
     items = pp_glob(items, glob_replacements)
-    items= pp_country_from_timespan(items)
+    # get metadata from timespans
+    # (if it was me who photographed)
+    # if 'Hartmut' in items[len(items)-1]:
+    items = pp_metadata_from_timespan(items)
     print(items)
+    # now the specific filters
     for ix, it in enumerate(items):
-        if it == "Südkorea":
+        if 'From Slide' in it:
+            pp_dia(ix)
+            outitems = items
+        if it == "South Korea":
             outitems = pp_s_korea(items, it, ix)
-        if it == "Mark":
-            outitems = pp_mark(items, it, ix)
-        if it == "Marokko":
+        if it == "Morocco":
             outitems = pp_morocco(items, it, ix)
-        if it == "Schweiz":
-            outitems = pp_ch(items, it, ix)
+        if it == "Switzerland":
+            outitems = pp_ch_cantons(items, ix)
 
     if not outitems:
         print("Status line unfiltered.")
     else:
         # only now, we remove the dropped items
         for x in delx:
-            if x >= 0:
-                if outitems[x]:
-                    del outitems[x]
+            if 0 <= x <= len(outitems) and outitems[x]:
+                del outitems[x]
         print("Status line changed to:")
         print(outitems)
         items = outitems
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/pytest-3
+# import pytest
 import postprocess
+
+# These tests assume the example file postprocess.py
+# It's recommended to add/modify tests here for every addition or modification there.
 
 sep = "|"
 
@@ -44,96 +48,103 @@ def test_glob4():
 
 
 # we have to make up a test for every filtered item of input
-# now, we have Südkorea, Mark, Marokko, Schweiz
+# now, we have Dia, Südkorea, Mark, Marokko, Schweiz
+
+# Südkorea tests
+def test_slide1():
+    # we remove all info except creator from images that are duplicates of slides
+    input = "From Slide|SubLocation|Location|ProvinceState|Country|1.11.2001|Creator"
+    assert put_out(input) == "From Slide|Creator"
+
 
 # Südkorea tests
 def test_skorea1():
     # we generally want to omit the province in S Korea
-    # so the S Korea function should return Name|Location|Südkorea|1.11.2001|Creator
-    input = "Name|SubLocation|Location|ProvinceState|Südkorea|1.11.2001|Creator"
-    assert put_out(input) == "Name|Location|Südkorea|1.11.2001|Creator"
+    # so the S Korea function should return Name|SubLocation|Location|South Korea|1.11.2001|Creator
+    input = "Name|SubLocation|Location|ProvinceState|South Korea|1.11.2001|Creator"
+    assert put_out(input) == "Name|SubLocation|Location|South Korea|1.11.2001|Creator"
 
 
 def test_skorea2():
     # except when ProvinceState is Busan, then it should return
-    # Name|Busan|Südkorea|1.11.2001|Creator
-    input = "Name|SubLocation|Location|Busan|Südkorea|1.11.2001|Creator"
-    assert put_out(input) == "Name|Busan|Südkorea|1.11.2001|Creator"
+    # Name|Busan|South Korea|1.11.2001|Creator
+    for province in ["Busan", "Jeju", "Seoul"]:
+        input = "Name|SubLocation|Location|" + province + "|South Korea|1.11.2001|Creator"
+        assert put_out(input) == "Name|SubLocation|Location|" + province + "|South Korea|1.11.2001|Creator"
 
 
 def test_skorea3():
-    # except when ProvinceState is Seoul, then it should return
-    # Name|Seoul|Südkorea|1.11.2001|Creator
-    input = "Name|SubLocation|Location|Seoul|Südkorea|1.11.2001|Creator"
-    assert put_out(input) == "Name|Seoul|Südkorea|1.11.2001|Creator"
+    # tests landmark adding
+    input = "|Pungcheon|Location|ProvinceState|South Korea|1.11.2001|Creator"
+    assert put_out(input) == "Hahoe/Byeongsanseowon|Location|South Korea|1.11.2001|Creator"
 
 
-def test_skorea4():
-    # except when ProvinceState is Jeju, then it should return
-    # Name|Location|Jeju|Südkorea|1.11.2001|Creator
-    input = "Name|SubLocation|Location|Jeju|Südkorea|1.11.2001|Creator"
-    assert put_out(input) == "Name|Location|Jeju|Südkorea|1.11.2001|Creator"
-
-
-# Mark Brandenburg test
-def test_mark():
-    # the Mark function should return Name|Sublocation|Location (Mark)|Country|1.11.2001|Creator
-    input = "Name|SubLocation|Location|Mark|Country|1.11.2001|Creator"
-    assert put_out(input) == "Name|SubLocation|Location (Mark)|Country|1.11.2001|Creator"
-
-
-# Marokko test
+# Morocco test
 def test_morocco():
     # we omit the province
     # the Morocco function should return Name|SubLocation|Location|Marokko|1.11.2001|Creator
-    input = "Name|SubLocation|Location|ProvinceState|Marokko|1.11.2001|Creator"
-    assert put_out(input) == "Name|SubLocation|Location|Marokko|1.11.2001|Creator"
+    input = "Name|SubLocation|Location|ProvinceState|Morocco|1.11.2001|Creator"
+    assert put_out(input) == "Name|SubLocation|Location|Morocco|1.11.2001|Creator"
+
+
+def test_morocco2():
+    # tests landmark adding
+    input = "|SubLocation|M'Semrir|ProvinceState|Morocco|1.11.2001|Creator"
+    assert put_out(input) == "Gorges du Dades|SubLocation|M'Semrir|Morocco|1.11.2001|Creator"
+
+
+def test_morocco3():
+    # Marrakesh should keep the province
+    input = "|SubLocation|Marrakech|Marrakech|Morocco|1.11.2001|Creator"
+    assert put_out(input) == "|SubLocation|Marrakech|Marrakech|Morocco|1.11.2001|Creator"
 
 
 # Schweiz tests
 def test_ch1():
     # generally, we do not change anything...
-    # the Switzerland function should return "Name|SubLocation|Location|ProvinceState|Schweiz|1.11.2001|Creator" unchanged
-    input = "Name|SubLocation|Location|ProvinceState|Schweiz|1.11.2001|Creator"
+    # the Switzerland function should return "Name|SubLocation|Location|ProvinceState|Switzerland|1.11.2001|Creator" unchanged
+    input = "Name|SubLocation|Location|ProvinceState|Switzerland|1.11.2001|Creator"
     assert put_out(input) == input
 
 
 def test_ch2():
     # ...except when input is "Name|SubLocation|Location|Kanton Zürich|Schweiz|1.11.2001|Creator"
-    # then we want to see "Name|SubLocation|Location ZH|Schweiz|1.11.2001|Creator"
-    input = "Name|SubLocation|Location|Kanton Zürich|Schweiz|1.11.2001|Creator"
-    assert put_out(input) == "Name|SubLocation|Location ZH|Schweiz|1.11.2001|Creator"
+    # then we want to see "Name|SubLocation|Location ZH|1.11.2001|Creator"
+    for canton in postprocess.cantons.keys():
+        input = "Name|SubLocation|Location|Kanton " + canton + "|Switzerland|1.11.2001|Creator"
+        assert put_out(input) == "Name|SubLocation|Location " + postprocess.cantons.get(canton) + "|1.11.2001|Creator"
+
 
 def test_ch3():
     # same as 2, but with glob replacement ('Zurich Province'-> 'Zürich' and then as in #2 above)
-    input = "Name|SubLocation|Location|Kanton Zurich Province|Schweiz|1.11.2001|Creator"
-    assert put_out(input) == "Name|SubLocation|Location ZH|Schweiz|1.11.2001|Creator"
+    input = "Name|SubLocation|Location|Kanton Zurich Province|Switzerland|1.11.2001|Creator"
+    assert put_out(input) == "Name|SubLocation|Location ZH|1.11.2001|Creator"
+
 
 def test_ch4():
     # ...except when input is "Name|SubLocation|Zürich|Kanton Zürich|Schweiz|1.11.2001|Creator"
     # when the canton's name is in the city name
-    # then we want to see "Name|SubLocation|Zürich|Schweiz|1.11.2001|Creator"
-    input = "Name|SubLocation|Zürich|Kanton Zürich|Schweiz|1.11.2001|Creator"
-    assert put_out(input) == "Name|SubLocation|Zürich|Schweiz|1.11.2001|Creator"
-
-def test_ch5():
-    # ...except when input is "Name|SubLocation|Zürich|Kanton Zürich|Schweiz|1.11.2001|Creator"
-    # when the canton's name is in the city name
-    # then we want to see "Name|SubLocation|Zürich|Schweiz|1.11.2001|Creator"
-    input = "Name|SubLocation|Basel|Kanton Basel-Stadt|Schweiz|1.11.2001|Creator"
-    assert put_out(input) == "Name|SubLocation|Basel|Schweiz|1.11.2001|Creator"
+    # then we want to see "Name|SubLocation|Zürich|1.11.2001|Creator"
+    cities = ['Zürich', 'Basel', 'St. Gallen']
+    for city in cities:
+        for canton in postprocess.cantons.keys():
+            if city in canton:
+                input = "Name|SubLocation|" + city + "|Kanton " + canton + "|Switzerland|1.11.2001|Creator"
+                assert put_out(input) == "Name|SubLocation|" + city + "|1.11.2001|Creator"
 
 
-# date timeline tests
+# date timeline tests: they assume the matching timespans in postprocess.py
 def test_timeline1():
-    # this should return Portugal as country
-    input = "Name|SubLocation|Location|ProvinceState||16.2.2017|Creator"
-    assert put_out(input) == "Name|SubLocation|Location|ProvinceState|Portugal|16.2.2017|Creator"
+    # this should return USA as country
+    input = "Name|SubLocation|Location|ProvinceState||08-13-2012|Creator"
+    assert put_out(input) == "Name|SubLocation|Location|ProvinceState|USA|08-13-2012|Creator"
+
 
 def test_timeline2():
-    # this should return USA as country
-    input = "Name|SubLocation|Location|ProvinceState||1.10.2014|Creator"
-    assert put_out(input) == "Name|SubLocation|Location|ProvinceState|USA|1.10.2014|Creator"
+    # this should return USA as country, NY as state, 'In teh Catskills' as sublocation, but leave Location untouched
+    input = "||Location|||08-20-2012|Creator"
+    assert put_out(input) == "|In the Catskills|Location|NY|USA|08-20-2012|Creator"
+
 
 def test_timeline3():
     # incorrect/no date
@@ -141,19 +152,46 @@ def test_timeline3():
     input = "Name|SubLocation|Location|ProvinceState||Date|Creator"
     assert put_out(input) == input
 
+
 def test_timeline4():
     # too few items
     # this should return unchanged
     input = "Name|Date"
     assert put_out(input) == input
 
+
 def test_timeline5():
-    # minimal items
+    # minimal items, tests that we do not go beyond left border
+    # this will not work when there's more in timespan than the country only
+    # (practically, this is not happening, because we would feed it with more empty items...)
+    # see #8 for this
     # this should return USA as country
-    input = "|1.10.2014|Creator"
-    assert put_out(input) == "USA|1.10.2014|Creator"
+    input = "|05-01-2019|Creator"
+    assert put_out(input) == "USA|05-01-2019|Creator"
+
+
 def test_timeline6():
     # country name already present
     # this should return unchanged
-    input = "Name|SubLocation|Location|ProvinceState|USA|1.10.2014|Creator"
+    input = "Name|SubLocation|Location|ProvinceState|USA|05-01-2019|Creator"
     assert put_out(input) == input
+
+
+def test_timeline7():
+    # compare this to #5 to see the difference between missing and empty input
+    input = "|||||08-20-2012|Creator"
+    assert put_out(input) == "|In the Catskills||NY|USA|08-20-2012|Creator"
+
+
+def test_timeline8():
+    # this should return no values except 'From Slide|Creator'
+    input = "|SubLocation|Location|ProvinceState|Country|10-16-2020|Creator"
+    assert put_out(input) == "From Slide|Creator"
+
+# def test_timeline9():
+#    with pytest.raises(ValueError, match=r"Too many items in timespan:*"):
+# too many timespan entries test
+# add the line below to the timespans before running this
+# '19141008': {'19141008': {'USA': {'PA': {'Philadelphia': {'30th Street Station':{ 'Something':{ 'This here is too much': None}}}}}}},
+#        input = "||||8.10.1914|Creator"
+#        print(put_out(input))
